6bf6f039c925378ee72b5086516a818a25cfa03a
luccadias <luccadiass@gmail.com>
2024-05-20 19:50:33 -0300

    feat: add TextField, Button, List and contents

--- pick-a-pal/ContentView.swift
+++ pick-a-pal/ContentView.swift
@@ -8,12 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var names: [String] = []
+    @State private var nameToAdd: String = ""
+    @State private var pickedName: String = ""
+    @State private var shouldRemovePickedName: Bool = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
+            
+            VStack (spacing: 8) {
+                Image(systemName: "person.3.sequence.fill")
+                    .foregroundStyle(.tint)
+                    .symbolRenderingMode(.hierarchical)
+                Text("Picka-a-Pal")
+            }
+            .font(.title)
+            .bold()
+            
+            Text(pickedName.isEmpty ? "" : pickedName)
+                .font(.title2)
+                .bold()
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            List {
+                ForEach(names, id: \.self) { name in
+                    Text(name)
+                }
+            }
+            TextField("Add Name", text: $nameToAdd)
+                .autocorrectionDisabled()
+                .onSubmit {
+                    if !nameToAdd.isEmpty {
+                        names.append(nameToAdd)
+                        nameToAdd = ""
+                    }
+                }
+            
+            Divider()
+            
+            Toggle("Remove when picked", isOn: $shouldRemovePickedName)
+            
+            Button("Pick Random Name") {
+                if let randomName = names.randomElement() {
+                    pickedName = randomName
+                    
+                    if shouldRemovePickedName {
+                        names.removeAll { name in
+                            return (name == randomName)
+                        }
+                    }
+                    
+                } else {
+                    pickedName = ""
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
         }
         .padding()
     }
