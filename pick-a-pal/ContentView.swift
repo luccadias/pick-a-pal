fdea7d237bfa99fa86f47d74e487b1bfcd380a49
luccadias <luccadiass@gmail.com>
2024-05-20 19:11:39 -0300

    feat: add pick-a-pal init project

---
+++ pick-a-pal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pick-a-pal
+//
+//  Created by Lucca Dias on 20/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pick-a-pal/pick_a_palApp.swift
@@ -0,0 +1,17 @@
+//
+//  pick_a_palApp.swift
+//  pick-a-pal
+//
+//  Created by Lucca Dias on 20/05/24.
+//
+
+import SwiftUI
+
+@main
+struct pick_a_palApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
